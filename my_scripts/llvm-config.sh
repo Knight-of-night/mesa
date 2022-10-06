997cb2e8910dd199909c0df75e15d739968d75e4
Knight-of-night <61961828+Knight-of-night@users.noreply.github.com>
2022-10-07 00:34:43 +0800

    Use llvm14

--- my_scripts/llvm-config.sh
+++ my_scripts/llvm-config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-Result = `llvm-config $* | sed -e 's/\/usr/\/usr\/x86_64-w64-mingw32/g;s/\.so/.dll/g'`
+Result = `llvm-config-14 $* | sed -e 's/\/usr/\/usr\/x86_64-w64-mingw32/g;s/\.so/.dll/g'`
 echo $Result
